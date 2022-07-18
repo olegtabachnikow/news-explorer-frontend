d08d303100513989a36868cf450936a9bf544f37
OlegTabachnikow <eskelll4ik@gmail.com>
2022-07-19 02:25:48 +0300

    baseurl

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://api.news-explorer-oleg.students.nomoredomainssbs.ru";
 function checkResponce(res) {
   if (res.ok) {
     return res.json();
